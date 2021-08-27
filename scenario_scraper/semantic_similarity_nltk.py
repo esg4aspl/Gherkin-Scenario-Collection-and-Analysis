f2aae744f457176303265dad738952406ec8b2c6
umutek <numutekici@gmail.com>
2021-08-27 14:44:16 +0300

    added scenario matcher

---
+++ scenario_scraper/semantic_similarity_nltk.py
@@ -0,0 +1,35 @@
+import nltk, string
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+nltk.download('punkt') # if necessary...
+
+
+stemmer = nltk.stem.porter.PorterStemmer()
+remove_punctuation_map = dict((ord(char), None) for char in string.punctuation)
+
+
+def stem_tokens(tokens):
+    return [stemmer.stem(item) for item in tokens]
+
+
+'''remove punctuation, lowercase, stem'''
+def normalize(text):
+    return stem_tokens(nltk.word_tokenize(text.lower().translate(remove_punctuation_map)))
+
+
+vectorizer = TfidfVectorizer(tokenizer=normalize, stop_words='english')
+
+
+def cosine_sim(text1, text2, verbose=None):
+    tfidf = vectorizer.fit_transform([text1, text2])
+    result = ((tfidf * tfidf.T).A)[0,1]
+    if verbose:
+        print(result)
+    return result
+
+
+def get_pairwise_similarity_form_corpus(corpus):
+    vect = TfidfVectorizer(min_df=1, stop_words="english")
+    tfidf = vect.fit_transform(corpus)
+    pairwise_similarity = tfidf * tfidf.T
+    return pairwise_similarity.A
